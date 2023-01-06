3d9ecb6823ec494492b5b95904dd551c1b0fe0c2
Yul310 <hsy3102@gmail.com>
2023-01-06 11:33:36 -0500

    pop updated

--- LinkedList.py
+++ LinkedList.py
@@ -32,23 +32,57 @@ class LinkedList:
 
     def pop(self):
         if self.length == 0:
-            print("nothing in the list")
-            return False
-        elif self.length == 1:
-            self.head = None
-        else:
-           
-            nextnum = self.head.next
+            return None
 
-            print("nextnum",nextnum)
-            while nextnum:
-                if nextnum.next== None:
-                    print("here")
-                    self.tail = self.head
-                    nextnum == None
-                    self.length -= 1
-                
-                self.head = self.head.next
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        
+        pre.next = None
+        self.tail = pre
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        print("temp",temp.value)
+        return temp
+
+
+
+
+
+
+        # if self.length == 0:
+        #     print("nothing in the list")
+        #     return False
+        # elif self.length == 1:
+        #     self.head = None
+        # else:
+        #     while self.head.next:
+        #         pre = self.head 
+        #         temp = pre.next
+
+        #         if temp == None:
+        #             print("last pre",pre.value)
+        #             self.tail= pre
+        #             self.tail.next == None
+        #             print("last tail",self.tail.value)
+        #             self.length -= 1
+        #             return
+
+        #         pre = temp
+        #         temp = temp.next
+        #         print("pre",pre.value)
+        #         print("tail",self.tail.value)
+
+
+            
+               
 
                 
 
@@ -58,6 +92,10 @@ class LinkedList:
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
-my_linked_list.pop()
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+# my_linked_list.pop()
 my_linked_list.printList()
 # print(my_linked_list.head.value)
