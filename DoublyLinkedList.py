34ab1196cc5a861d4751853046b04398263e4c8d
Yul310 <hsy3102@gmail.com>
2023-01-23 15:49:16 -0500

    update

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -3,10 +3,11 @@ class Node:
         self.value = value
         self.next = None
         self.prev = None
+       
 
 
 class DoublyLinkedList:
-    
+
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -19,6 +20,46 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.length ==0:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.tail
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        
+        else:
+           
+            former = self.tail.prev
+            former.next = None
+            temp.prev = None
+            self.tail = former
+
+    
+        self.length -= 1
+        print(temp.value)
+        return temp.value
+
 my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list.append(5)
+my_doubly_linked_list.append(9)
+my_doubly_linked_list.print_list()
+print("haha")
+my_doubly_linked_list.pop()
 my_doubly_linked_list.print_list()
 
