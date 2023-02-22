396fa51f5d388b011b09fa0baf69d7b9d8562235
Yul310 <hsy3102@gmail.com>
2023-02-21 23:50:14 -0500

    delete method added

--- RecursiveBinarySearchTree.py
+++ RecursiveBinarySearchTree.py
@@ -74,6 +74,19 @@ class BinarySearchTree:
             self.root = Node(value)
         self.__r_insert(self.root, value)
 
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left,value)
+        elif value > current_node.value:
+            current_node.left = self.__delete_node(current_node.right,value)
+        else:
+            return current_node
+
+        return current_node
+
+
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
