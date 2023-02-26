e388c28457f08a1f2a06786293641da036acff5e
Yul310 <hsy3102@gmail.com>
2023-02-25 23:52:05 -0500

    selection sort added

--- BasicSort.py
+++ BasicSort.py
@@ -5,9 +5,29 @@ def bubble_sort(my_list):
                 temp = my_list[j]
                 my_list[j] = my_list[j+1]
                 my_list[j+1] = temp
+        print("process",my_list)
 
     return my_list
 
 
 
 print(bubble_sort([4,2,6,5,1,3]))
+
+
+def selection_sort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+        
+    return my_list
+
+
+print(selection_sort([4,2,6,5,1,3]))
+
+        
