a1cf780651cc240e0eacf08d122472cf22ef5323
Yul310 <hsy3102@gmail.com>
2023-04-23 23:49:31 -0400

    dfs postorder updated

--- DfsPreOrder.py
+++ DfsPreOrder.py
@@ -68,6 +68,25 @@ class BinarySearchTree:
 
 
 # /////////////////////////////////////////////////////////
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+    
+        traverse(self.root)
+        return results
+
+
+
+
+
+
+
+# /////////////////////////////////////////////////////////
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
